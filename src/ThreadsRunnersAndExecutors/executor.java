daf191a95ea5f8c33a3f3aa098109eaf958f7f07
SFerrel <ferrelspencer@gmail.com>
2017-06-10 17:17:22 -0600

    edited a few things

--- src/ThreadsRunnersAndExecutors/executor.java
+++ src/ThreadsRunnersAndExecutors/executor.java
@@ -4,14 +4,14 @@ package ThreadsRunnersAndExecutors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-/** Just a basic executor
+/** Just a basic executor with at fixed thread pool
  * Created by SFerrel on 6/10/2017.
  */
 public class executor{
     public static void main(String[] args) throws InterruptedException{
 
         ExecutorService executorService = Executors.newFixedThreadPool(100);
-        for(int i=0; i<=10; i++){
+        for(int i=0; i<=3; i++){
             executorService.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -21,7 +21,7 @@ public class executor{
                     System.out.println("HoLa");
                     Thread.currentThread().sleep(200);
                 }catch (Exception e){
-
+                        //leaving this blank.
                     }
             }});
         }
