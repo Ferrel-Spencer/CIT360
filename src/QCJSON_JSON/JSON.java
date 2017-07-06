b4ef8e34854dd02a24187e9d99738b5892c9aefc
SFerrel <ferrelspencer@gmail.com>
2017-07-06 16:54:43 -0600

    added libraries and created JSON example

--- src/QCJSON_JSON/JSON.java
+++ src/QCJSON_JSON/JSON.java
@@ -1,9 +1,72 @@
 package QCJSON_JSON;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+
+
 /**
- * Created by SFerrel on 7/6/2017.
+ * Created by SFerrel on 7/6/2017. Found this example at--> https://www.tutorialspoint.com/json/json_java_example.htm
+ * I've been playing with it and changing the values in it to understand what happens
  */
 public class JSON {
+    public static void main(String[] args) throws IOException, ParseException {
+        JSONObject obj = new JSONObject();
+
+        obj.put("name", "finny");
+        obj.put("num of fins", new Integer(2));
+        obj.put("Species", "Catfish");
+        obj.put("Swims upside down?", new Boolean(true));
+
+        System.out.print(obj);
+
+        //StringWriter
+        StringWriter out = new StringWriter();
+        obj.writeJSONString(out);
+        String jsonText = out.toString();
+        System.out.print(jsonText);
+
+        //Decoding JSON example with JSONObject and JSONArray
+        JSONParser parser = new JSONParser();
+        String list = "[ 0, {\"One\":{\"Two\":{\"Three\":{\"Four\":{\"Five\":{\"Six\":\"Seven\"}}}}}}]";
+
+        try {
+            Object obj2 = parser.parse(list);
+            JSONArray array = (JSONArray) obj2;
+
+            System.out.println();
+            System.out.println("Second element");
+            System.out.println(array.get(1));
+            System.out.println();
 
 
+            JSONObject obj3 = (JSONObject) array.get(1);
+            System.out.println("Field \"1\"");
+            System.out.println(obj3.get("One"));
+
+
+            list = "{     }"; // not sure what this does here I believe this is a map
+            obj2 = parser.parse(list);
+            System.out.println(obj2);
+
+            list = "[\"Five\", \" Oh \"]"; // okay so anything string wise needs to have \" \" which turns into "" to get it to be parsed. I think this is the array
+            obj2 = parser.parse(list);
+            System.out.println(obj2);
+
+            list = "[\"One\",\"Two\",\"Three\",\"Four\"]";
+            obj2 = parser.parse(list);
+            System.out.println(obj2);
+
+        } catch (ParseException pe) {
+
+            System.out.println("position: " + pe.getPosition());
+            System.out.println(pe);
+
+
+        }
+    }
 }
